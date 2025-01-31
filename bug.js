6d19a1b192811b21c48f628febc915efe50d399c
BugHunter-X <askzenai@gmail.com>
2025-01-31 21:56:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.writeHead(200);
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+console.log('Server is running on port 8080');
+//The server might not be able to start due to port 8080 being used by another application.
